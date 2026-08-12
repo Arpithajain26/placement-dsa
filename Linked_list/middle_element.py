263446598cf66d3fe7ada138bb2a19a474c7b44b
Arpitha Jain <arpithaammujain39@gmail.com>
2026-08-13 04:10:45 +0530

    added new file

---
+++ Linked_list/middle_element.py
@@ -0,0 +1,56 @@
+"""Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100"""
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        count=0
+        temp=head
+        while temp!=None:
+            count+=1
+            temp=temp.next
+        mid=count//2
+        temp=head
+        while temp!=None:
+            if mid==0:
+                return temp
+            mid-=1
+            temp=temp.next
+        
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+solution = Solution()
+
+result = solution.middleNode(head)
+
+print(result.val)
